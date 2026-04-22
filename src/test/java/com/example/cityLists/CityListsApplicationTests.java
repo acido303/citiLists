ef3a998b155a4fc670e62cbe50c75a902aaa1063
acido404 <acido303@gmail.com>
2026-04-22 10:21:18 +0100

    Created application using Spring Initializr https://start.spring.io/

---
+++ src/test/java/com/example/cityLists/CityListsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cityLists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityListsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
